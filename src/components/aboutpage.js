7ea3cba035a41377127746c910b4bcd4aa757237
Andrew Pedersen <39564158+andimalfresh@users.noreply.github.com>
2020-10-07 00:16:38 -0400

    yea

--- src/components/aboutpage.js
+++ src/components/aboutpage.js
@@ -4,18 +4,23 @@ import "../App.css";
 const aboutPage = () => {
   return (
     <div>
-        <div className="pageImage">
-          <img src={require("./img/Space.jpg")} />
-        </div>
-        <div className="pageText">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </div>
+      <div className="pageImage">
+        <img  className="pageImage" src={require("./img/Space.jpg")} />
+      </div>
+      <div className="pageText">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+        <a href="/">
+        <button> Go to Page </button>
+      </a>
+      </div>
+
+
     </div>
   );
 };
--- src/components/homepage.js
+++ src/components/homepage.js
@@ -15,7 +15,11 @@ const homePage = () => {
         velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
         occaecat cupidatat non proident, sunt in culpa qui officia deserunt
         mollit anim id est laborum.
+        <a href="/aboutPage">
+        <button> Go to Page </button>
+      </a>
       </div>
+
     </div>
   );
 };
